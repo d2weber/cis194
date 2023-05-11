cad0eb9d18133b62122272dfb29291145d7705de
d2weber <29163905+d2weber@users.noreply.github.com>
2023-05-11 09:17:03 +0200

    Complete week-03

---
+++ week-03/Golf.hs
@@ -0,0 +1,33 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Golf where
+
+import Data.List (tails, transpose)
+
+skips :: [a] -> [[a]]
+skips l =
+  [ [c | (c, ii) <- zip l [1 ..], ii `mod` i == 0]
+    | i <- [1 .. length l]
+  ]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima l = [b | (a, b, c) <- wins, a < b, b > c]
+  where
+    wins = zip3 l (drop 1 l) (drop 2 l)
+
+-- alternative wit generalized windowing function
+window :: Int -> [a] -> [[a]]
+window n = foldr (zipWith (:)) (repeat []) . take n . tails
+
+localMaxima2 :: [Integer] -> [Integer]
+localMaxima2 l = [b | [a, b, c] <- window 3 l, a < b, b > c]
+
+histogram :: [Integer] -> String
+histogram lst =
+  unlines $
+    reverse
+      ( [['0' .. '9'], replicate 10 '=']
+          ++ [ [if i `elem` line then '*' else '.' | i <- [0 .. 9]]
+               | line <- transpose $ [filter (== i) lst | i <- [0 .. 9]]
+             ]
+      )
